b87b0e736bf16cbeb81063e7cefae8ee9850663b
Albina Zakirova <zak1804@gmail.com>
2018-11-10 15:21:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.collect_with_index do |element, index|
+  element.unshift('#{index+1}.')
+  puts array
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.each do |ithem|
+    ithem.capitalize << "!"
+    
 end
 
 def long_planeteer_calls# code an argument here
