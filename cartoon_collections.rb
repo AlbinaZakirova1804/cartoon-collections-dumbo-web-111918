c05946f7711fa18498d8c485c284b9b5eaface72
Albina Zakirova <zak1804@gmail.com>
2018-11-10 15:34:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,11 @@ end
 def summon_captain_planet(array)
   array.each do |ithem|
     ithem.capitalize << "!"
-    
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+array.any? do |element|
+  element.length >
 end
 
 def find_the_cheese# code an argument here
